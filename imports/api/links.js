685c8ee681c13349f9f5ce713378ee341a015f45
kev-inc <kevinchanxy@gmail.com>
2020-04-01 11:30:30 +0800

    backend created, graph implemented

--- imports/api/links.js
+++ imports/api/links.js
@@ -3,3 +3,8 @@ import { Mongo } from 'meteor/mongo';
 export const LinksCollection = new Mongo.Collection('links');
 
 export const TempCollection = new Mongo.Collection('temps');
+
+// export function getRoom(roomId) {
+//     console.log(roomId)
+//     return TempCollection.find({'roomId':roomId})
+// }
