bb22e1bd54c3547aba08f0ac388bccf5275a82c9
kev-inc <kevinchanxy@gmail.com>
2020-04-01 23:40:11 +0800

    themed the graph to follow specifications

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -5,9 +5,7 @@ import Graph from './Graph.jsx';
 
 export const App = () => (
   <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
     <Graph/>
   </div>
 );
+
--- imports/ui/Graph.jsx
+++ imports/ui/Graph.jsx
@@ -1,17 +1,49 @@
 import React, { useEffect } from 'react'
+import { makeStyles } from '@material-ui/core/styles';
 import Dygraph from 'dygraphs'
 import { useTracker } from 'meteor/react-meteor-data';
 import { TempCollection } from '../api/links';
 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid'
+import orange from '@material-ui/core/colors/orange'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+      
+    },
+    appbar: {
+        backgroundColor: orange[700],
+        color: 'black'
+    },
+    card: {
+        backgroundColor: 'white',
+        padding: 12
+    },
+    title: {
+      flexGrow: 1,
+    },
+    graphRef: {
+        height: 200
+    },
+    graphLabels: {
+        display: 'inline-block'
+    }
+  }));
+
 export const Graph = () => {
+    const classes = useStyles()
     const temps = useTracker(() => {
-        return TempCollection.find({}, {limit: 100}).map(temp => {
+        return TempCollection.find({}, { limit: 100 }).map(temp => {
             return [
                 temp.timestamp,
-                temp['0'], 
-                temp['1'], 
-                temp['2'], 
-                temp['3'], 
+                temp['0'],
+                temp['1'],
+                temp['2'],
+                temp['3'],
                 temp['4'],
                 temp['5'],
                 temp['6']
@@ -21,40 +53,45 @@ export const Graph = () => {
 
     useEffect(() => {
         new Dygraph(document.getElementById('graphRef'), temps, {
-            labels:["Timestamp", "Room 0", "Room 1", "Room 2", "Room 3", "Room 4", "Room 5", "Room 6"],
+            labels: ["Timestamp", "Room 0", "Room 1", "Room 2", "Room 3", "Room 4", "Room 5", "Room 6"],
             hideOverlayOnMouseOut: false,
             labelsDiv: document.getElementById('graphLabels'),
             labelsSeparateLines: true,
             legend: 'always',
             axes: {
                 y: {
-                    valueFormatter: (num) => num.toPrecision(3)+"°C",
-                    axisLabelFormatter: (num) => num.toPrecision(2)+"°C",
+                    valueFormatter: (num) => num.toPrecision(3) + "°C",
+                    axisLabelFormatter: (num) => num.toPrecision(2) + "°C",
                 }
             }
         })
 
     })
 
-    return(
-        <div style={styles.root} >
-            <div id="graphLabels" style={styles.graphLabels} align="right"></div>
-            <div id='graphRef' style={styles.graphRef}></div>
+    return (
+        <div className={classes.root}>
+            <AppBar position="static" className={classes.appbar}>
+                <Toolbar>
+                    <Typography variant="h6" className={classes.title}>
+                        Temperature
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+
+            <div className={classes.card} >
+                <Grid container>
+                    <Grid item xs={2}>
+                        <div id="graphLabels" align="right"></div>
+                    </Grid>
+                    <Grid item xs={10}>
+                        <div id='graphRef' className={classes.graphRef}></div>
+                    </Grid>
+                </Grid>
+            </div>
         </div>
+
     )
 }
 
-const styles = {
-    root: {
-        display: 'flex'
-    },
-    graphRef: {
-        width: '80%',
-        height: 200
-    },
-    graphLabels: {
-        width: '20%'
-    }
-}
 
 export default Graph
