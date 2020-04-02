b7a905218de1da5a117398cc0c36e41452d7ec78
Tan Yikai <tanyikai1994@gmail.com>
2020-04-02 17:42:42 +0800

    Removed unnecessary imports

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
 import Graph from './Graph.jsx';
 import { DateInput } from './DateInput';
 import { FloorplanSVG } from './FloorplanSVG';
