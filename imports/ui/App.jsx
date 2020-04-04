e7c8b8628ae4be749c5daf1bc8578ac094243dcb
kev-inc <kevinchanxy@gmail.com>
2020-04-04 12:39:09 +0800

    integrated datepicker with graph

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Graph from './Graph.jsx';
 import { DateInput } from './DateInput';
 import { FloorplanSVG } from './FloorplanSVG';
 import { SampleSlider } from './SampleSlider';
- 
-export const App = () => (
-  <div>
-    <DateInput />
-    <SampleSlider/>
-    <Graph />
-    <FloorplanSVG />
-  </div>
-);
+
+export const App = () => {
+  
+  const [startDate, setStartDate] = useState(new Date("10/02/2013"));
+  const [endDate, setEndDate] = useState(new Date("10/04/2013"));
+
+  return (
+    <div>
+      <DateInput 
+        startDate={startDate}
+        setStartDate={setStartDate}
+        endDate={endDate}
+        setEndDate={setEndDate}
+      />
+      <SampleSlider />
+      <Graph 
+        startDate={startDate}
+        endDate={endDate}
+      />
+      <FloorplanSVG />
+    </div>
+  )
+}
 
